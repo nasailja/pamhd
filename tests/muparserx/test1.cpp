648a43e65016ad974de4d7ec2bbf2f1c062e7cb3
Ilja Honkonen <ilja.j.honkonen@nasa.gov>
2014-07-09 18:33:20 -0400

    Modify muxparser test to parse expressions outputting more than one value.
    https://code.google.com/p/muparserx/issues/detail?id=35

--- tests/muparserx/test1.cpp
+++ tests/muparserx/test1.cpp
@@ -29,7 +29,7 @@ using namespace std;
 
 int main()
 {
-	mup::ParserX parser(mup::pckALL_NON_COMPLEX);
+	mup::ParserX parser(mup::pckCOMMON | mup::pckNON_COMPLEX | mup::pckMATRIX | mup::pckUNIT);
 	mup::Value value1{0};
 	mup::Variable variable1{&value1};
 	parser.DefineVar("v1", variable1);
@@ -45,30 +45,66 @@ int main()
 			<< "Wrong value for sum: " << sum
 			<< ", should be 32"
 			<< endl;
+		abort();
 	}
 
 
 	mup::Value value2{3, 0};
 	mup::Variable variable2{&value2};
 	parser.DefineVar("v2", variable2);
-	parser.SetExpr("sqrt(v2[0]*v2[0] + v2[1]*v2[1] + v2[2]*v2[2])");
+	parser.SetExpr("{v2[0]*v2[0], v2[1]*v2[1], v2[2]*v2[2]}");
 
-	sum = 0;
+	// values of variables used in expression above
 	const std::vector<std::array<double, 3>> test_vectors{
 		{1, 3, 5},
-		{7, 11, 13}
+		{7, 11, 13},
+		{-7, -11, -13}
 	};
-	for (const auto v: test_vectors) {
-		for (size_t i = 0; i < v.size(); i++) {
-			value2.At(i) = v[i];
+
+	for (const auto test_vector: test_vectors) {
+		for (size_t i = 0; i < test_vector.size(); i++) {
+			value2.At(i) = test_vector[i];
 		}
-		sum += parser.Eval().GetFloat();
-	}
-	if (int(sum) != 24) {
-		std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
-			<< "Wrong value for int(sum): " << sum
-			<< ", should be 24"
-			<< endl;
+
+		const auto& evaluated
+			= [&parser](){
+				try {
+					return parser.Eval().GetArray();
+				} catch(mup::ParserError &error) {
+					std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+						<< "Could not evaluate expression "
+						<< parser.GetExpr() << ": " << error.GetMsg()
+						<< endl;
+					abort();
+				}
+			}();
+
+		if (evaluated.GetRows() != 1) {
+			std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+				<< "Invalid number of rows in expression " << parser.GetExpr()
+				<< ": " << evaluated.GetRows() << ", should be 1"
+				<< endl;
+			abort();
+		}
+		if (evaluated.GetCols() != 3) {
+			std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+				<< "Invalid number of columns in expression " << parser.GetExpr()
+				<< ": " << evaluated.GetCols() << ", should be 3"
+				<< endl;
+			abort();
+		}
+
+		for (int i = 0; i < evaluated.GetCols(); i++) {
+			const double value = evaluated.At(i).GetFloat();
+			if (test_vector[i]*test_vector[i] != value) {
+				std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+					<< "Invalid evaluated value at index "
+					<< i << ": " << value
+					<< ", should be: " << test_vector[i]*test_vector[i]
+					<< endl;
+				abort();
+			}
+		}
 	}
 
 	return 0;
