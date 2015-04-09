3f0921b5d7a3ef90a624cb00a3225aacfc8766ef
Ilja Honkonen <ilja.j.honkonen@nasa.gov>
2015-04-09 17:44:38 -0400

    Add a test for curl calculation in a periodic grid.

---
+++ tests/divergence/get1d_curl_periodic.cpp
@@ -0,0 +1,259 @@
+/*
+Tests vector field curl calculation of PAMHD in 1d.
+
+Copyright 2015 Ilja Honkonen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+* Neither the names of the copyright holders nor the names of their contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#include "array"
+#include "cstdlib"
+#include "iostream"
+#include "limits"
+#include "tuple"
+#include "vector"
+
+#include "dccrg.hpp"
+#include "dccrg_cartesian_geometry.hpp"
+
+#include "divergence/remove.hpp"
+
+// returns x component of function
+double function(const double z)
+{
+	return std::sin(z);
+}
+
+// returns y component of curl
+double curl_of_function(const double z)
+{
+	return std::cos(z);
+}
+
+
+struct Vector {
+	using data_type = std::array<double, 3>;
+};
+
+struct Curl {
+	using data_type = std::array<double, 3>;
+};
+
+using Cell = gensimcell::Cell<
+	gensimcell::Always_Transfer,
+	Vector,
+	Curl
+>;
+
+
+/*!
+Returns maximum norm if p == 0.
+*/
+template<class Grid_T> double get_diff_lp_norm(
+	const std::vector<uint64_t>& cells,
+	const Grid_T& grid,
+	const double p,
+	const double cell_volume
+) {
+	double local_norm = 0, global_norm = 0;
+	for (const auto& cell: cells) {
+		const auto* const cell_data = grid[cell];
+		if (cell_data == NULL) {
+			std::cerr << __FILE__ << ":" << __LINE__
+				<< ": No data for cell " << cell
+				<< std::endl;
+			abort();
+		}
+
+		const double curl_of = curl_of_function(grid.geometry.get_center(cell)[2]);
+
+		if (p == 0) {
+			local_norm = std::max(
+				local_norm,
+				std::fabs((*cell_data)[Curl()][1] - curl_of)
+			);
+		} else {
+			local_norm += std::pow(
+				std::fabs((*cell_data)[Curl()][1] - curl_of),
+				p
+			);
+		}
+	}
+	local_norm *= cell_volume;
+
+	if (p == 0) {
+		MPI_Comm comm = grid.get_communicator();
+		MPI_Allreduce(&local_norm, &global_norm, 1, MPI_DOUBLE, MPI_MAX, comm);
+		MPI_Comm_free(&comm);
+		return global_norm;
+	} else {
+		MPI_Comm comm = grid.get_communicator();
+		MPI_Allreduce(&local_norm, &global_norm, 1, MPI_DOUBLE, MPI_SUM, comm);
+		MPI_Comm_free(&comm);
+		return std::pow(global_norm, 1.0 / p);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
+		std::cerr << "Couldn't initialize MPI." << std::endl;
+		abort();
+	}
+
+	MPI_Comm comm = MPI_COMM_WORLD;
+
+	int rank = 0, comm_size = 0;
+	if (MPI_Comm_rank(comm, &rank) != MPI_SUCCESS) {
+		std::cerr << "Couldn't obtain MPI rank." << std::endl;
+		abort();
+	}
+	if (MPI_Comm_size(comm, &comm_size) != MPI_SUCCESS) {
+		std::cerr << "Couldn't obtain size of MPI communicator." << std::endl;
+		abort();
+	}
+
+
+	// intialize Zoltan
+	float zoltan_version;
+	if (Zoltan_Initialize(argc, argv, &zoltan_version) != ZOLTAN_OK) {
+		std::cerr << "Zoltan_Initialize failed." << std::endl;
+		abort();
+	}
+
+	const unsigned int neighborhood_size = 0;
+	const int max_refinement_level = 0;
+
+	double
+		old_norm = std::numeric_limits<double>::max();
+	size_t old_nr_of_cells = 0;
+	for (size_t nr_of_cells = 8; nr_of_cells <= 1024; nr_of_cells *= 2) {
+
+		dccrg::Dccrg<Cell, dccrg::Cartesian_Geometry> grid;
+
+		const std::array<uint64_t, 3> grid_size{{1, 1, nr_of_cells}};
+
+		if (
+			not grid.initialize(
+				grid_size,
+				comm,
+				"RANDOM",
+				neighborhood_size,
+				max_refinement_level,
+				true,
+				false,
+				false
+			)
+		) {
+			std::cerr << __FILE__ << ":" << __LINE__
+				<< ": Couldn't initialize grid."
+				<< std::endl;
+			abort();
+		}
+
+		dccrg::Cartesian_Geometry::Parameters geom_params;
+		geom_params.start = {{0, 0, 0}};
+		geom_params.level_0_cell_length = {{1, 1, 2 * M_PI / nr_of_cells}};
+
+		if (not grid.set_geometry(geom_params)) {
+			std::cerr << __FILE__ << ":" << __LINE__
+				<< ": Couldn't set grid geometry."
+				<< std::endl;
+			abort();
+		}
+
+		grid.balance_load();
+
+		const auto cells = grid.get_cells();
+		for (const auto& cell: cells) {
+			auto* const cell_data = grid[cell];
+			if (cell_data == NULL) {
+				std::cerr << __FILE__ << ":" << __LINE__
+					<< ": No data for cell " << cell
+					<< std::endl;
+				abort();
+			}
+
+			(*cell_data)[Vector()][0] = function(grid.geometry.get_center(cell)[2]);
+		}
+		grid.update_copies_of_remote_neighbors();
+
+		pamhd::divergence::get_curl(
+			cells,
+			grid,
+			[](Cell& cell_data) -> Vector::data_type& {
+				return cell_data[Vector()];
+			},
+			[](Cell& cell_data) -> Curl::data_type& {
+				return cell_data[Curl()];
+			}
+		);
+
+		const double
+			p_of_norm = 0,
+			norm = get_diff_lp_norm(cells, grid, p_of_norm, 2 * M_PI / nr_of_cells);
+
+		if (norm > old_norm) {
+			if (grid.get_rank() == 0) {
+				std::cerr << __FILE__ << ":" << __LINE__
+					<< ": X norm with " << nr_of_cells
+					<< " cells " << norm
+					<< " is larger than with " << nr_of_cells / 2
+					<< " cells " << old_norm
+					<< std::endl;
+			}
+			abort();
+		}
+
+		if (old_nr_of_cells > 0) {
+			const double
+				order_of_accuracy
+					= -log(norm / old_norm)
+					/ log(double(nr_of_cells) / old_nr_of_cells);
+
+			if (order_of_accuracy < 0.9) {
+				if (grid.get_rank() == 0) {
+					std::cerr << __FILE__ << ":" << __LINE__
+						<< ": Order of accuracy from "
+						<< old_nr_of_cells << " to " << nr_of_cells
+						<< " is too low for x: " << order_of_accuracy
+						<< std::endl;
+				}
+				abort();
+			}
+		}
+
+		old_nr_of_cells = nr_of_cells;
+		old_norm = norm;
+	}
+
+	MPI_Finalize();
+
+	return EXIT_SUCCESS;
+}
