63d25860c6f426908e9d7360374907e04ca6de04
Ilja Honkonen <ilja.j.honkonen@nasa.gov>
2015-05-25 09:43:01 -0400

    Add missing include to mhd reference test.

--- tests/mhd/reference.cpp
+++ tests/mhd/reference.cpp
@@ -37,6 +37,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "gensimcell.hpp"
 
+#include "mhd/common.hpp"
 #include "mhd/hll_athena.hpp"
 #include "mhd/hlld_athena.hpp"
 #include "mhd/roe_athena.hpp"
