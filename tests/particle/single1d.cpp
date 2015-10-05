b609749c0c51ba374524acc37e7a16e431bb515f
Ilja Honkonen <ilja.j.honkonen@nasa.gov>
2015-10-05 10:55:03 -0400

    Change options in particle/single1d.cpp for easier sampling of parameter space.

--- tests/particle/single1d.cpp
+++ tests/particle/single1d.cpp
@@ -33,6 +33,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "cmath"
 #include "cstdlib"
 #include "fstream"
+#include "ios"
 #include "iostream"
 #include "random"
 
@@ -51,7 +52,8 @@ constexpr double
 	electron_c2m = electron_charge / electron_mass,
 	vacuum_permittivity = 8.854187817e-12,
 	light_speed = 299792458,
-	light_speed2 = light_speed*light_speed;
+	light_speed2 = light_speed*light_speed,
+	max_particle_speed = 0.999999999 * light_speed;
 
 
 using namespace std;
@@ -96,35 +98,35 @@ struct Fields
 		inv_wavelength_ends(1.0 / given_wavelength_ends)
 	{}
 
-	std::string print() const
+	double get_base_arg(const double time, const double position) const
 	{
-		std::string ret_val("2*pi*x*");
-		ret_val += boost::lexical_cast<std::string>(this->inv_wavelength_ends);
-		ret_val += " - 2*pi*t*";
-		ret_val += boost::lexical_cast<std::string>(frequency);
-		ret_val += " + (1/";
-		ret_val += boost::lexical_cast<std::string>(1.0/this->inv_wavelength_ends);
-		ret_val += " - 1/";
-		ret_val += boost::lexical_cast<std::string>(1.0/this->inv_wavelength_mid);
-		ret_val += ")*2*";
-		ret_val += boost::lexical_cast<std::string>(this->length);
-		ret_val += "*cos(pi*x/";
-		ret_val += boost::lexical_cast<std::string>(this->length);
-		ret_val += ")";
-		return ret_val;
+		return
+			2 * M_PI * position * this->inv_wavelength_ends
+			- 2 * M_PI * frequency * time
+			+ (this->inv_wavelength_ends - this->inv_wavelength_mid)
+				* 2 * this->length
+				* cos(M_PI * position / this->length);
 	}
 
 	std::array<Eigen::Vector3d, 2> operator()(
 		const double time,
-		const double position
+		double position
 	) const {
-		const double
-			arg
-				= 2 * M_PI * position * this->inv_wavelength_ends
-				- 2 * M_PI * frequency * time
-				+ (this->inv_wavelength_ends - this->inv_wavelength_mid)
-					* 2 * this->length
-					* cos(M_PI * position / this->length);
+		const double arg
+			= [&](){
+				if (position >= 0) {
+					return this->get_base_arg(time, position);
+				} else {
+					position -= this->length;
+					// make sure arg approaches same value when position -> 0
+					return
+						this->get_base_arg(time, position)
+						+ 2 * this->length * (
+							M_PI / this->inv_wavelength_ends
+							+ 2 * (this->inv_wavelength_ends - this->inv_wavelength_mid)
+						);
+				}
+			}();
 
 		const Eigen::Vector3d
 			E{
@@ -177,7 +179,11 @@ struct Propagator
 
 int main(int argc, char* argv[])
 {
+	using std::acos;
+	using std::cos;
+	using std::minmax;
 	using std::pow;
+	using std::sin;
 	using std::sqrt;
 
 	if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
@@ -211,22 +217,26 @@ int main(int argc, char* argv[])
 	bool verbose = true;
 	std::string outfile_name("out");
 	double
-		length = 1e5,
+		length = 100,
 		time_start = 0,
 		time_length = 1e4,
 		save_dt = 0,
-		save_dx = 0,
+		save_dx = 1,
 		time_step_factor = 1,
 		em_frequency = 1e4,
-		density_mid = 1e7,
+		density_mid = 1e8,
 		density_ends = 1e8,
 		b0 = 1e-6,
-		b1 = 1e-7,
-		v_min = 1e6,
-		v_max = 1e8,
-		angle_min = 0,
-		angle_max = 180;
-	size_t particles = 1, seed = 1;
+		b1 = 1e-10,
+		v_par_min = 1e7,
+		v_par_max = 1e7,
+		b_angle_min = 0,
+		b_angle_max = 1e-3,
+		e_angle_min = 0,
+		e_angle_max = 180,
+		ExB_sign = 0,
+		particles = 1;
+	size_t seed = 1;
 
 	boost::program_options::options_description
 		options("Usage: program_name [options], where options are");
@@ -265,31 +275,43 @@ int main(int argc, char* argv[])
 		("B0",
 			boost::program_options::value<double>(&b0)
 				->default_value(b0),
-			"Magnetic field parallel to simulation box")
+			"Magnetic field component parallel to x coordinate")
 		("B1",
 			boost::program_options::value<double>(&b1)
 				->default_value(b1),
 			"Magnetic field magnitude perpendicular to simulation box")
 		("particles",
-			boost::program_options::value<size_t>(&particles)
+			boost::program_options::value<double>(&particles)
 				->default_value(particles),
 			"Approximate total number of particles to propagate (minimum is 1 / MPI rank)")
-		("v-min",
-			boost::program_options::value<double>(&v_min)
-				->default_value(v_min),
-			"Minimum velocity of electrons (m/s)")
-		("v-max",
-			boost::program_options::value<double>(&v_max)
-				->default_value(v_max),
-			"Maximum velocity of electrons (m/s)")
-		("angle-min",
-			boost::program_options::value<double>(&angle_min)
-				->default_value(angle_min),
-			"Minimum angle of electron velocity from x axis (degrees)")
-		("angle-max",
-			boost::program_options::value<double>(&angle_max)
-				->default_value(angle_max),
-			"Maximum angle of electron velocity from x axis (degrees)")
+		("v-par-min",
+			boost::program_options::value<double>(&v_par_min)
+				->default_value(v_par_min),
+			"Minimum magnitude of velocity of electrons parallel to magnetic field (> 0, < c, m/s)")
+		("v-par-max",
+			boost::program_options::value<double>(&v_par_max)
+				->default_value(v_par_max),
+			"Maximum magnitude of velocity of electrons parallel to magnetic field (> 0, < c, m/s)")
+		("B-angle-min",
+			boost::program_options::value<double>(&b_angle_min)
+				->default_value(b_angle_min),
+			"Minimum angle of electron velocity away from magnetic field direction (degrees)")
+		("B-angle-max",
+			boost::program_options::value<double>(&b_angle_max)
+				->default_value(b_angle_max),
+			"Maximum angle of electron velocity away from magnetic field direction (degrees)")
+		("E-angle-min",
+			boost::program_options::value<double>(&e_angle_min)
+				->default_value(e_angle_min),
+			"Minimum angle of electron velocity away from electric field direction (degrees)")
+		("E-angle-max",
+			boost::program_options::value<double>(&e_angle_max)
+				->default_value(e_angle_max),
+			"Maximum angle of electron velocity away from electric field direction (degrees)")
+		("ExB-sign",
+			boost::program_options::value<double>(&ExB_sign)
+				->default_value(ExB_sign),
+			"Sign of electron velocity component parallel to ExB (0 == all or < 0 or > 0)")
 		("save-dt",
 			boost::program_options::value<double>(&save_dt)
 				->default_value(save_dt),
@@ -317,9 +339,11 @@ int main(int argc, char* argv[])
 			option_variables
 		);
 	} catch (std::exception& e) {
-		std::cerr <<  __FILE__ << "(" << __LINE__ << "): "
-			<< "Couldn't parse command line options: " << e.what()
-			<< std::endl;
+		if (rank == 0) {
+			std::cerr <<  __FILE__ << "(" << __LINE__ << "): "
+				<< "Couldn't parse command line options: " << e.what()
+				<< std::endl;
+		}
 		MPI_Finalize();
 		return EXIT_FAILURE;
 	}
@@ -337,14 +361,72 @@ int main(int argc, char* argv[])
 		verbose = false;
 	}
 
-	angle_min *= M_PI / 180;
-	angle_max *= M_PI / 180;
-	if (v_max >= light_speed) {
-		v_max = 0.999999 * light_speed;
+	if (b_angle_min < 0) {
+		if (rank == 0) {
+			std::cerr << "Minimum angle from magnetic field must be >= 0." << std::endl;
+		}
+		MPI_Finalize();
+		return EXIT_FAILURE;
 	}
+	if (b_angle_max > 180) {
+		if (rank == 0) {
+			std::cerr << "Maximum angle from magnetic field must be <= 180." << std::endl;
+		}
+		MPI_Finalize();
+		return EXIT_FAILURE;
+	}
+
+	if (e_angle_min < 0) {
+		if (rank == 0) {
+			std::cerr << "Minimum angle from electric field must be >= 0." << std::endl;
+		}
+		MPI_Finalize();
+		return EXIT_FAILURE;
+	}
+	if (e_angle_max > 180) {
+		if (rank == 0) {
+			std::cerr << "Maximum angle from electric field must be <= 180." << std::endl;
+		}
+		MPI_Finalize();
+		return EXIT_FAILURE;
+	}
+
+	std::tie(v_par_min, v_par_max) = minmax(abs(v_par_min), abs(v_par_max));
+	b_angle_min *= M_PI / 180;
+	b_angle_max *= M_PI / 180;
+	e_angle_min *= M_PI / 180;
+	e_angle_max *= M_PI / 180;
+
+	if (e_angle_max + b_angle_max < M_PI / 2) {
+		if (rank == 0) {
+			std::cerr << "Sum of maximum angles away from B and E must be >= 90 degrees." << std::endl;
+		}
+		MPI_Finalize();
+		return EXIT_FAILURE;
+	}
+	if (e_angle_min + b_angle_min < M_PI / 2) {
+		if (rank == 0) {
+			std::cout << "NOTE: Sum of minimum angles away from B and E < 90 degrees might slow things down significantly." << std::endl;
+		}
+	}
+
+	if (v_par_min / abs(cos(b_angle_min)) >= light_speed) {
+		if (rank == 0) {
+			std::cerr << "Minimum electron velocity >= light speed." << std::endl;
+		}
+		MPI_Finalize();
+		return EXIT_FAILURE;
+	}
+	if (v_par_max / abs(cos(b_angle_max)) >= light_speed) {
+		if (rank == 0) {
+			std::cout << "NOTE: Maximum electron velocity >= light speed might slow things down significantly." << std::endl;
+		}
+	}
+
+	particles = std::round(particles);
 	seed += rank;
 	if (particles > comm_size) {
-		particles /= comm_size;
+		particles = round(particles / comm_size);
 	} else {
 		particles = 1;
 	}
@@ -355,6 +437,9 @@ int main(int argc, char* argv[])
 		cerr << "Couldn't open file " << outfile_name << " for writing." << endl;
 		return EXIT_FAILURE;
 	}
+	outfile
+		<< std::scientific
+		<< std::setprecision(std::numeric_limits<double>::max_digits10);
 
 	// derived parameters
 	const double
@@ -406,16 +491,6 @@ int main(int argc, char* argv[])
 
 	const double min_wavelength = std::min(wavelength_mid, wavelength_ends);
 
-	const Fields fields(
-		length,
-		em_frequency,
-		e1,
-		b0,
-		b1,
-		wavelength_mid,
-		wavelength_ends
-	);
-
 	if (verbose) {
 		outfile
 			<< "# Parameters\n# Start time (s): " << time_start
@@ -423,10 +498,12 @@ int main(int argc, char* argv[])
 			<< time_length * e_gyro_freq << " electron gyro periods (g_e)"
 			<< "\n# Simulation box length (m): " << length
 			<< "\n# Number of particles: " << particles
-			<< "\n# Minimum electron velocity (m/s): " << v_min
-			<< "\n# Maximum electron velocity (m/s): " << v_max
-			<< "\n# Minimum electron velocity angle from x axis (m/s): " << angle_min
-			<< "\n# Maximum electron velocity angle from x axis (m/s): " << angle_max
+			<< "\n# Minimum electron velocity parallel to magnetic field (m/s): " << v_par_min
+			<< "\n# Maximum electron velocity parallel to magnetic field (m/s): " << v_par_max
+			<< "\n# Minimum electron velocity angle away from magnetic field (degrees): " << b_angle_min * 180 / M_PI
+			<< "\n# Maximum electron velocity angle away from magnetic field (degrees): " << b_angle_max * 180 / M_PI
+			<< "\n# Minimum electron velocity angle away from electric field (degrees): " << e_angle_min * 180 / M_PI
+			<< "\n# Maximum electron velocity angle away from electric field (degrees): " << e_angle_max * 180 / M_PI
 			<< "\n# EM wave frequency (f_w): " << em_frequency
 			<< " Hz\n# Electron number density in middle (1/m^3): " << density_mid
 			<< "\n# Electron number density at ends (1/m^3): " << density_ends
@@ -450,53 +527,137 @@ int main(int argc, char* argv[])
 			<< "\n# EM phase speed at ends: " << wavelength_ends * em_frequency
 			<< " m/s, " << wavelength_ends * em_frequency / light_speed << " c"
 			<< "\n# E1 (V/m): " << e1
-			<< "\n# Argument to E (0, E1*sin(arg), E1*sin(arg+pi/2) "
-				"and B (B0, B1*sin(arg-pi/2), B1*sin(arg): " << fields.print()
-			<< "\n# Each particle on separate line, format: r_x, r_y, ..., v_y, v_z, r_x, ..."
+			<< "\n# Each particle on separate line, format: "
+			   "r1x, v1x, v1y, v1z, Ey(r1), Ez(r1), Bx(r1), By(r1), Bz(r1), r2x..."
 			<< std::endl;
 	}
-
-	Propagator propagator(electron_c2m, fields);
-	runge_kutta_fehlberg78<state_t> stepper;
 
 	std::mt19937_64 random_source;
 	random_source.seed(seed);
 	std::uniform_real_distribution<>
-		v_gen(v_min, v_max),
-		angle_gen(angle_min, angle_max),
-		// rotation of velocity about x axis
-		rotation_gen(0, 2 * M_PI);
+		v_par_gen(v_par_min, v_par_max),
+		b_angle_gen(b_angle_min, b_angle_max),
+		e_angle_gen(e_angle_min, e_angle_max),
+		v_sign_gen(0, 1);
 
-	for (size_t particle = 0; particle < particles; particle++) {
+	const Fields fields(
+		length,
+		em_frequency,
+		e1,
+		b0,
+		b1,
+		wavelength_mid,
+		wavelength_ends
+	);
+
+	const Eigen::Vector3d
+		initial_pos{0, 0, 0},
+		init_E = fields(0, initial_pos[0])[0],
+		init_B = fields(0, initial_pos[0])[1];
+
+	Propagator propagator(electron_c2m, fields);
+	runge_kutta_fehlberg78<state_t> stepper;
+
+	size_t particle = 0;
+	while (particle < particles) {
 
 		const auto
-			v = v_gen(random_source),
-			angle = angle_gen(random_source),
-			rotation = rotation_gen(random_source);
+			e_angle = e_angle_gen(random_source),
+			b_angle = b_angle_gen(random_source);
+		if (b_angle + e_angle < M_PI / 2) {
+			continue;
+		}
 
-		Eigen::Vector3d initial_vel{
-			v * std::cos(angle),
-			v * std::sin(angle) * std::sin(rotation),
-			v * std::sin(angle) * std::cos(rotation)
+		const auto
+			v_par = v_par_gen(random_source),
+			v = v_par / abs(cos(b_angle));
+		if (v >= max_particle_speed) {
+			continue;
+		}
+
+		const Eigen::Vector3d v_in_E_B_ExB{
+			v * cos(e_angle),
+
+			v * cos(b_angle),
+
+			[&]() -> double {
+				const auto arg_to_sqrt
+					= pow(sin(e_angle) * sin(b_angle), 2)
+					- pow(cos(e_angle) * cos(b_angle), 2);
+				if (arg_to_sqrt < 0) {
+					return 0;
+				} else {
+					const auto ret_val = v * sqrt(arg_to_sqrt);
+					// select which side of EB plane velocity will be on
+					if (ExB_sign < 0) {
+						return -ret_val;
+					}
+					if (ExB_sign > 0) {
+						return +ret_val;
+					}
+
+					auto v_sign = v_sign_gen(random_source);
+					if (v_sign < 0.5) {
+						return -ret_val;
+					} else {
+						return +ret_val;
+					}
+				}
+			}()
 		};
+		if (v_in_E_B_ExB.norm() >= max_particle_speed) {
+			continue;
+		}
+
+		/*
+		Rotate initial velocity to x, y, z coordinate system
+		*/
+
+		const Eigen::Vector3d
+			old_x = init_E.normalized(),
+			old_y = init_B.normalized(),
+			old_z = init_E.cross(init_B).normalized(),
+			new_x = Eigen::Vector3d::UnitX(),
+			new_y = Eigen::Vector3d::UnitY(),
+			new_z = Eigen::Vector3d::UnitZ();
+
+		// http://ocw.mit.edu/courses/aeronautics-and-astronautics/16-07-dynamics-fall-2009/lecture-notes/MIT16_07F09_Lec03.pdf
+		Eigen::Matrix3d transformer;
+		transformer <<
+			new_x.dot(old_x), new_x.dot(old_y), new_x.dot(old_z),
+			new_y.dot(old_x), new_y.dot(old_y), new_y.dot(old_z),
+			new_z.dot(old_x), new_z.dot(old_y), new_z.dot(old_z);
+
+		const Eigen::Vector3d initial_vel = transformer * v_in_E_B_ExB;
+		if (initial_vel.norm() >= max_particle_speed) {
+			continue;
+		}
+
+		const auto angle_V_E = acos(initial_vel.normalized().dot(init_E.normalized()));
+		if (angle_V_E < e_angle_min or angle_V_E > e_angle_max)  {
+			continue;
+		}
+		const auto angle_V_B = acos(initial_vel.normalized().dot(init_B.normalized()));
+		if (angle_V_B < b_angle_min or angle_V_B > b_angle_max)  {
+			continue;
+		}
 
 		state_t state{
-			Eigen::Vector3d{0, 0, 0},
+			initial_pos,
 			initial_vel
 		};
-
-		if (initial_vel[0] < 0) {
-			state[0][0] = length;
-		}
 
 		std::vector<double> data_to_save;
 		if (save_dt >= 0 or save_dx >= 0) {
 			data_to_save.push_back(state[0][0]);
-			data_to_save.push_back(state[0][1]);
-			data_to_save.push_back(state[0][2]);
 			data_to_save.push_back(state[1][0]);
 			data_to_save.push_back(state[1][1]);
 			data_to_save.push_back(state[1][2]);
+			data_to_save.push_back(init_E[1]);
+			data_to_save.push_back(init_E[2]);
+			data_to_save.push_back(init_B[0]);
+			data_to_save.push_back(init_B[1]);
+			data_to_save.push_back(init_B[2]);
 		}
 
 		double
@@ -517,16 +678,20 @@ int main(int argc, char* argv[])
 		double previous_save_x = state[0][0];
 		while (true) {
 
+			const auto EB = fields(simulation_time, state[0][0]);
 			if (
 				simulation_time >= next_save_t
 				or abs(previous_save_x - state[0][0]) >= save_dx
 			) {
 				data_to_save.push_back(state[0][0]);
-				data_to_save.push_back(state[0][1]);
-				data_to_save.push_back(state[0][2]);
 				data_to_save.push_back(state[1][0]);
 				data_to_save.push_back(state[1][1]);
 				data_to_save.push_back(state[1][2]);
+				data_to_save.push_back(EB[0][1]);
+				data_to_save.push_back(EB[0][2]);
+				data_to_save.push_back(EB[1][0]);
+				data_to_save.push_back(EB[1][1]);
+				data_to_save.push_back(EB[1][2]);
 
 				if (save_dt == 0) {
 					next_save_t += time_length;
@@ -537,7 +702,6 @@ int main(int argc, char* argv[])
 			}
 
 			double max_time_step = std::numeric_limits<double>::max();
-			const auto EB = fields(simulation_time, state[0][0]);
 			max_time_step = std::min(
 				max_time_step,
 				pamhd::particle::get_minmax_step(
@@ -553,11 +717,34 @@ int main(int argc, char* argv[])
 			max_time_step = std::min(max_time_step, 1 / em_frequency / 8);
 			dt = max_time_step * time_step_factor;
 
+			if (not std::isfinite(state[0][0])) {
+				std::cerr << "before: "
+					<< state[0][0] << ", "
+					<< state[0][1] << ", "
+					<< state[0][2] << ";  "
+					<< state[1][0] << ", "
+					<< state[1][1] << ", "
+					<< state[1][2]
+					<< std::endl;
+				abort();
+			}
+
 			stepper.do_step(propagator, state, simulation_time, dt);
-			if (state[0][0] < -0.01 * length or state[0][0] > 1.01 * length) {
+			if (state[0][0] < -1.001 * length or state[0][0] > 1.001 * length) {
 				break;
 			}
 
+			if (not std::isfinite(state[0][0])) {
+				std::cerr << "after: "
+					<< state[0][0] << ", "
+					<< state[0][1] << ", "
+					<< state[0][2] << ";  "
+					<< state[1][0] << ", "
+					<< state[1][1] << ", "
+					<< state[1][2]
+					<< std::endl;
+				abort();
+			}
 			simulation_time += dt;
 
 			if (simulation_time > time_start + time_length) {
@@ -575,19 +762,25 @@ int main(int argc, char* argv[])
 			}
 		}
 
+		const auto EB = fields(simulation_time, state[0][0]);
 		if (save_dt >= 0 or save_dx >= 0) {
 			data_to_save.push_back(state[0][0]);
-			data_to_save.push_back(state[0][1]);
-			data_to_save.push_back(state[0][2]);
 			data_to_save.push_back(state[1][0]);
 			data_to_save.push_back(state[1][1]);
 			data_to_save.push_back(state[1][2]);
+			data_to_save.push_back(EB[0][1]);
+			data_to_save.push_back(EB[0][2]);
+			data_to_save.push_back(EB[1][0]);
+			data_to_save.push_back(EB[1][1]);
+			data_to_save.push_back(EB[1][2]);
 		}
 
 		for (const auto& item: data_to_save) {
 			outfile << item << " ";
 		}
 		outfile << "\n";
+
+		particle++;
 	}
 	if (not outfile.good()) {
 		cerr << "Writing results to " << outfile_name << " probably failed." << endl;
