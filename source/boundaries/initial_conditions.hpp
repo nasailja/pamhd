26e79312f9ad9a3f025e35c0b84062ca86b411bb
Ilja Honkonen <ilja.j.honkonen@nasa.gov>
2016-05-24 10:48:56 -0400

    Add a class for handling all initial conditions of a simulation.
    Move common initial condition code to boundaries/common.hpp.

---
+++ source/boundaries/initial_conditions.hpp
@@ -0,0 +1,213 @@
+/*
+Class for handling all initial conditions of a simulation.
+
+Copyright 2016 Ilja Honkonen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+* Neither the names of the copyright holders nor the names of their contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef PAMHD_BOUNDARIES_INITIAL_CONDITIONS_HPP
+#define PAMHD_BOUNDARIES_INITIAL_CONDITIONS_HPP
+
+
+#include "algorithm"
+#include "array"
+#include "stdexcept"
+#include "string"
+#include "type_traits"
+#include "utility"
+
+#include "rapidjson/document.h"
+#include "mpParser.h"
+
+#include "boundaries/geometries.hpp"
+#include "boundaries/initial_condition.hpp"
+#include "boundaries/math_expression.hpp"
+
+
+namespace pamhd {
+namespace boundaries {
+
+
+/*!
+Collection of boundaries creatable from arguments given on command line.
+*/
+template<
+	class Geometry_Id,
+	class Variable
+> class Initial_Conditions
+{
+public:
+
+	/*!
+	Prepares initial conditions from given rapidjson object.
+
+	Given object must have a "default" key with corresponding
+	value as a number, string (math expression), or object
+	(3d point cloud data), \see Initial_Condition::set() for
+	details.
+
+	Example json object:
+	\verbatim
+	{
+		"default": "sin(2*pi*x/123)",
+		"regions": [
+			{"geometry_id": 0, "value": 123}
+			{"geometry_id": 1, "value": "sin(t)"}
+		]
+	}
+	\endverbatim
+	*/
+	void set(const rapidjson::Value& object)
+	{
+		if (not object.HasMember("default")) {
+			throw std::invalid_argument(__FILE__ ": object doesn't have a default key.");
+		}
+		const auto& default_ = object["default"];
+
+		try {
+			fill_variable_value_from_json(
+				default_,
+				this->default_init_cond_type,
+				this->default_number_value,
+				this->default_math_expression,
+				this->default_coordinates,
+				this->default_coordinate_type,
+				this->default_data
+			);
+		} catch (const std::invalid_argument& error) {
+			throw std::invalid_argument(
+				std::string(__FILE__ "(") + std::to_string(__LINE__) + ") "
+				+ " Couldn't set variable from json object: "
+				+ error.what()
+			);
+		}
+
+		if (not object.HasMember("regions")) {
+			return;
+		}
+		const auto& json_regions = object["regions"];
+
+		if (not json_regions.IsArray()) {
+			throw std::invalid_argument(__FILE__ ": regions is not an array.");
+		}
+
+		this->regions.clear();
+		this->regions.resize(json_regions.Size());
+
+		for (size_t i = 0; i < json_regions.Size(); i++) {
+			this->regions[i].set(json_regions[i]);
+		}
+	}
+
+
+	typename Variable::data_type get_default_data(
+		const double& t,
+		const double& x,
+		const double& y,
+		const double& z,
+		const double& radius,
+		const double& latitude,
+		const double& longitude
+	) {
+		switch (this->default_init_cond_type) {
+		case 1:
+			return this->default_number_value;
+
+		case 2:
+			return this->default_math_expression.evaluate(
+				t, x, y, z, radius, latitude, longitude
+			);
+
+		// nearest neighbor interpolation in point cloud
+		case 3:
+			return find_and_get_data<Variable>(
+				[&]()
+					-> std::array<double, 3>
+				{
+					switch (this->default_coordinate_type) {
+					case 1:
+						return {x, y, z};
+					case 2:
+						return {radius, latitude, longitude};
+					default:
+						throw std::out_of_range(__FILE__ ": Invalid coordinate type.");
+					}
+				}(),
+				this->default_coordinates,
+				this->default_data
+			);
+
+		default:
+			throw std::out_of_range(__FILE__ ": Invalid initial condition type.");
+		}
+	}
+
+
+	size_t get_number_of_regions() const
+	{
+		return this->regions.size();
+	}
+
+
+	typename Variable::data_type get_data(
+		const size_t& region_index,
+		const double& t,
+		const double& x,
+		const double& y,
+		const double& z,
+		const double& radius,
+		const double& latitude,
+		const double& longitude
+	) {
+		return this->regions[region_index].get_data(
+			t, x, y, z, radius, latitude, longitude
+		);
+	}
+
+
+private:
+
+	/*
+	Variables for handling default initial condition, see
+	corresponding names in initial_condition.hpp for more info
+	*/
+	typename Variable::data_type default_number_value;
+	Math_Expression<Variable> default_math_expression;
+	std::array<std::vector<double>, 3> default_coordinates;
+	int default_coordinate_type = -1;
+	std::vector<typename Variable::data_type> default_data;
+	int default_init_cond_type = -1;
+
+	// regions of non-default initial condition loaded from json data.
+	std::vector<Initial_Condition<Geometry_Id, Variable>> regions;
+
+};
+
+}} // namespaces
+
+#endif // ifndef PAMHD_BOUNDARIES_INITIAL_CONDITIONS_HPP
---
+++ source/boundaries/math_expression.hpp
@@ -0,0 +1,378 @@
+/*
+Class for evaluating math expression of a simulation variable.
+
+Copyright 2016 Ilja Honkonen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+* Neither the names of the copyright holders nor the names of their contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef PAMHD_MATH_EXPRESSION_HPP
+#define PAMHD_MATH_EXPRESSION_HPP
+
+
+#include "stdexcept"
+#include "string"
+#include "type_traits"
+#include "utility"
+
+#include "mpParser.h"
+
+#include "boundaries/common.hpp"
+
+
+namespace pamhd {
+namespace boundaries {
+
+
+/*!
+Variables available in expression, all are of floating point type:
+	- t: simulation time
+	- x, y, z: center of simulation cell in cartesian coordinates
+	- radius, lat, lon: center in spherical coordinates
+*/
+template<class Variable> class Math_Expression
+{
+public:
+
+	Math_Expression() :
+		t_var(&t_val),
+		x_var(&x_val), y_var(&y_val), z_var(&z_val),
+		radius_var(&radius_val), lat_var(&lat_val), lon_var(&lon_val)
+	{
+		this->parser.DefineVar("t", this->t_var);
+		this->parser.DefineVar("x", this->x_var);
+		this->parser.DefineVar("y", this->y_var);
+		this->parser.DefineVar("z", this->z_var);
+		this->parser.DefineVar("radius", this->radius_var);
+		this->parser.DefineVar("lat", this->lat_var);
+		this->parser.DefineVar("lon", this->lon_var);
+	}
+
+	Math_Expression(const Math_Expression& other) :
+		parser(other.parser),
+		t_val(other.t_val),
+		x_val(other.x_val), y_val(other.y_val), z_val(other.z_val),
+		radius_val(other.radius_val), lat_val(other.lat_val), lon_val(other.lon_val),
+		t_var(&t_val),
+		x_var(&x_val), y_var(&y_val), z_var(&z_val),
+		radius_var(&radius_val), lat_var(&lat_val), lon_var(&lon_val)
+	{}
+
+	Math_Expression(Math_Expression&& other) = delete;
+
+
+	void set_expression(const std::string& expression)
+	{
+		this->parser.SetExpr(expression);
+	}
+
+	std::string get_expression() const
+	{
+		return this->parser.GetExpr();
+	}
+
+
+	typename Variable::data_type evaluate(
+		const double& t,
+		const double& x,
+		const double& y,
+		const double& z,
+		const double& radius,
+		const double& latitude,
+		const double& longitude
+	) {
+		this->t_val = t;
+		this->x_val = x;
+		this->y_val = y;
+		this->z_val = z;
+		this->radius_val = radius;
+		this->lat_val = latitude;
+		this->lon_val = longitude;
+		return this->evaluate_impl();
+	}
+
+
+private:
+
+	mup::ParserX parser = mup::ParserX(mup::pckCOMMON | mup::pckNON_COMPLEX | mup::pckMATRIX | mup::pckUNIT);
+	mup::Value t_val, x_val, y_val, z_val, radius_val, lat_val, lon_val;
+	mup::Variable t_var, x_var, y_var, z_var, radius_var, lat_var, lon_var;
+
+
+	//! used to evaluate math expression if Variable::data_type is integral
+	template<class V = Variable> typename std::enable_if<
+		std::is_integral<typename V::data_type>::value,
+		typename V::data_type
+	>::type evaluate_impl()
+	{
+		try {
+			const auto& evaluated = this->parser.Eval();
+			if (evaluated.GetType() != 'i' and evaluated.GetType() != 'f') {
+				throw std::invalid_argument(
+					std::string("Expression \"")
+					+ this->parser.GetExpr()
+					+ std::string("\" is neither integer nor floating point.")
+				);
+			}
+
+			return evaluated.GetInteger();
+
+		} catch(mup::ParserError &error) {
+			std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+				<< "Could not evaluate expression \"" << this->parser.GetExpr()
+				<< "\" for variable " << Variable::get_name()
+				<< ": " << error.GetMsg()
+				<< std::endl;
+			throw;
+		}
+	}
+
+
+	//! used to evaluate math expression if Variable::data_type is floating point
+	template<class V = Variable> typename std::enable_if<
+		std::is_floating_point<typename V::data_type>::value,
+		typename V::data_type
+	>::type evaluate_impl()
+	{
+		try {
+			const auto& evaluated = this->parser.Eval();
+			if (evaluated.GetType() != 'i' and evaluated.GetType() != 'f') {
+				throw std::invalid_argument(
+					std::string("Expression \"")
+					+ this->parser.GetExpr()
+					+ std::string("\" is neither integer nor floating point.")
+				);
+			}
+
+			return evaluated.GetFloat();
+
+		} catch(mup::ParserError &error) {
+			std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+				<< "Could not evaluate expression \"" << this->parser.GetExpr()
+				<< "\" for variable " << Variable::get_name()
+				<< ": " << error.GetMsg()
+				<< std::endl;
+			throw;
+		}
+	}
+
+
+	//! used to evaluate math expression if Variable::data_type is array of floating point numbers
+	template<class V = Variable> typename std::enable_if<
+		std::tuple_size<typename V::data_type>::value >= 0
+			&& std::is_floating_point<typename V::data_type::value_type>::value,
+		typename V::data_type
+	>::type evaluate_impl()
+	{
+		const auto& evaluated
+			= [this](){
+				try {
+					const auto& temp = this->parser.Eval();
+					if (temp.GetType() != 'm') {
+						throw std::invalid_argument(
+							std::string("Expression \"")
+							+ this->parser.GetExpr()
+							+ std::string("\" is not an array.")
+						);
+					}
+
+					return temp.GetArray();
+
+				} catch(mup::ParserError &error) {
+					std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+						<< "Could not evaluate expression \"" << this->parser.GetExpr()
+						<< "\" for variable " << Variable::get_name()
+						<< ": " << error.GetMsg()
+						<< std::endl;
+					throw;
+				}
+			}();
+
+		// TODO: merge with identical code in int version
+		if (evaluated.GetRows() != 1) {
+			throw std::invalid_argument(
+				std::string("Invalid number of rows in expression \"")
+				+ parser.GetExpr()
+				+ std::string("\" for variable ")
+				+ Variable::get_name()
+				+ std::string(", should be 1\n")
+			);
+		}
+
+		constexpr size_t size = std::tuple_size<typename V::data_type>::value;
+
+		if (evaluated.GetCols() != size) {
+			throw std::invalid_argument(
+				std::string("Invalid number of columns in expression \"")
+				+ parser.GetExpr()
+				+ std::string("\" for variable ")
+				+ Variable::get_name()
+				+ std::string("\n")
+			);
+		}
+
+		typename Variable::data_type ret_val;
+		for (size_t i = 0; i < size; i++) {
+			ret_val[i] = evaluated.At(int(i)).GetFloat();
+		}
+
+		return ret_val;
+	}
+
+
+	//! used to evaluate math expression if Variable::data_type is array of integral numbers
+	template<class V = Variable> typename std::enable_if<
+		std::tuple_size<typename V::data_type>::value >= 0
+			&& std::is_integral<typename V::data_type::value_type>::value,
+		typename V::data_type
+	>::type evaluate_impl()
+	{
+		const auto& evaluated
+			= [this](){
+				try {
+					const auto& temp = this->parser.Eval();
+					if (temp.GetType() != 'm') {
+						throw std::invalid_argument(
+							std::string("Expression \"")
+							+ this->parser.GetExpr()
+							+ std::string("\" is not an array.")
+						);
+					}
+
+					return temp.GetArray();
+
+				} catch(mup::ParserError &error) {
+					std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+						<< "Could not evaluate expression \""
+						<< this->parser.GetExpr() << "\": " << error.GetMsg()
+						<< " for variable " << Variable::get_name()
+						<< std::endl;
+					throw;
+				}
+			}();
+
+		if (evaluated.GetRows() != 1) {
+			throw std::invalid_argument(
+				std::string("Invalid number of rows in expression \"")
+				+ parser.GetExpr()
+				+ std::string("\" for variable ")
+				+ Variable::get_name()
+				+ std::string(", should be 1\n")
+			);
+		}
+
+		constexpr size_t size = std::tuple_size<typename Variable::data_type>::value;
+
+		if (evaluated.GetCols() != size) {
+			throw std::invalid_argument(
+				std::string("Invalid number of columns in expression \"")
+				+ parser.GetExpr()
+				+ std::string("\" for variable ")
+				+ Variable::get_name()
+				+ std::string("\n")
+			);
+		}
+
+		typename Variable::data_type ret_val;
+		for (size_t i = 0; i < size; i++) {
+			ret_val[i] = evaluated.At(int(i)).GetInteger();
+		}
+
+		return ret_val;
+	}
+
+
+	#ifdef EIGEN_WORLD_VERSION
+
+	//! used to evaluate math expression if Variable::data_type is Eigen::Matrix<double or float, N, 1>
+	template<class V = Variable> typename std::enable_if<
+		V::data_type::ColsAtCompileTime == 1
+			&& V::data_type::RowsAtCompileTime >= 1
+			&& std::is_floating_point<typename V::data_type::value_type>::value,
+		typename V::data_type
+	>::type evaluate_impl()
+	{
+		const auto& evaluated
+			= [this](){
+				try {
+					const auto& temp = this->parser.Eval();
+					if (temp.GetType() != 'm') {
+						throw std::invalid_argument(
+							std::string("Expression ")
+							+ this->parser.GetExpr()
+							+ std::string(" is not an array.")
+						);
+					}
+
+					return temp.GetArray();
+
+				} catch(mup::ParserError &error) {
+					std::cerr <<  __FILE__ << "(" << __LINE__<< ") "
+						<< "Could not evaluate expression \""
+						<< this->parser.GetExpr() << "\": " << error.GetMsg()
+						<< " of variable " << Variable::get_name()
+						<< std::endl;
+					throw;
+				}
+			}();
+
+		if (evaluated.GetRows() != 1) {
+			throw std::invalid_argument(
+				std::string("Invalid number of rows in expression ")
+				+ parser.GetExpr()
+				+ std::string(" for variable ")
+				+ Variable::get_name()
+				+ std::string(", should be 1\n")
+			);
+		}
+
+		constexpr size_t size = V::data_type::RowsAtCompileTime;
+
+		if (evaluated.GetCols() != size) {
+			throw std::invalid_argument(
+				std::string("Invalid number of columns in expression ")
+				+ parser.GetExpr()
+				+ std::string(" for variable ")
+				+ Variable::get_name()
+				+ std::string("\n")
+			);
+		}
+
+		typename Variable::data_type ret_val;
+		for (size_t i = 0; i < size; i++) {
+			ret_val(i) = evaluated.At(int(i)).GetFloat();
+		}
+
+		return ret_val;
+	}
+
+	#endif // ifdef EIGEN_WORLD_VERSION
+};
+
+}} // namespaces
+
+#endif // ifndef PAMHD_MATH_EXPRESSION_HPP
