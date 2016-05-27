60740efbb84161c49500575801b8ebc0085a0498
Ilja Honkonen <ilja.j.honkonen@nasa.gov>
2016-05-27 12:26:55 -0400

    Add missing header to boundaries test program.

--- tests/boundaries/boundaries.cpp
+++ tests/boundaries/boundaries.cpp
@@ -40,6 +40,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "mpi.h"
 
 #include "boundaries/boundaries.hpp"
+#include "boundaries/geometries.hpp"
 
 
 using namespace pamhd::boundaries;
